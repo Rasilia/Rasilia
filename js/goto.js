59be6da76de3b4a44952ed11680b5f7c14ba189c
冬风凌天 <62973385+Rasilia@users.noreply.github.com>
2020-10-25 00:28:52 +0800

    Update

--- js/goto.js
+++ js/goto.js
@@ -2,7 +2,7 @@ if (navigator.appName == 'Netscape')
 var language = navigator.language;
 else
 var language = navigator.browserLanguage;
-if (language.indexOf('en') > -1) document.location.href = 'en/MP.html';
-else if (language.indexOf('zh') > -1) document.location.href = 'zh/MP.html';
+if (language.indexOf('en') > -1) document.location.href = 'Languages/en/MP.html';
+else if (language.indexOf('zh') > -1) document.location.href = 'Languages/zh/MP.html';
 else
-document.location.href = 'en/MP.html';
+document.location.href = 'Languages/en/MP.html';
