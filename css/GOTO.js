a461bba7dc99cbc992251d1fc418f5e6d9d4a129
冬风凌天 <62973385+Rasilia@users.noreply.github.com>
2020-10-25 00:04:48 +0800

    Delete GOTO.js

--- css/GOTO.js
+++
@@ -1,8 +0,0 @@
-if (navigator.appName == 'Netscape')
-var language = navigator.language;
-else
-var language = navigator.browserLanguage;
-if (language.indexOf('zh') >-1) document.location.href = 'zh.html';
-else if (language.indexOf('en') >-1) document.location.href = 'en.html'
-else
-document.location.href = 'en.html'
