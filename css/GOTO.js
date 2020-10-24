864f653b4e7414fc81d428593d651d1e2919f8ba
冬风凌天 <62973385+Rasilia@users.noreply.github.com>
2020-10-24 23:57:53 +0800

    Update GOTO.js

--- css/GOTO.js
+++ css/GOTO.js
@@ -2,6 +2,7 @@ if (navigator.appName == 'Netscape')
 var language = navigator.language;
 else
 var language = navigator.browserLanguage;
-if (language.indexOf('zh') >-1) document.location.href = 'zh_cn.html';
+if (language.indexOf('zh') >-1) document.location.href = 'zh.html';
+else if (language.indexOf('en') >-1) document.location.href = 'en.html'
 else
-document.location.href = 'zh_cn.html'
+document.location.href = 'en.html'
