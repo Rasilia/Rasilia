a1e72e08e690a92a30c9e6bc02ded2d44cb09777
冬风凌天 <62973385+Rasilia@users.noreply.github.com>
2020-10-26 23:35:15 +0800

    Update goto.js

--- js/goto.js
+++ js/goto.js
@@ -3,6 +3,6 @@ var language = navigator.language;
 else
 var language = navigator.browserLanguage;
 if (language.indexOf('en') > -1) document.location.href = 'lang/en/index.html';
-else if (language.indexOf('zh') > -1) document.location.href = 'lang/zh/index.html';
+else if (language.indexOf('zh') > -1) document.location.href = 'lang/zh/';
 else
 document.location.href = 'lang/en/index.html';
