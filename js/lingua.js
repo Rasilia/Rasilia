6177873febf26a8f8f0ddc76b57bf457a24d7a34
Rasilia-冬风 <62973385+Rasilia@users.noreply.github.com>
2022-06-28 07:02:32 +0800

    ~

--- js/lingua.js
+++ js/lingua.js
@@ -1,14 +1,27 @@
 function en() {
+    document.getElementsByClassName("selected")[0].className = "";
+    document.getElementById("en").className = "selected";
     document.getElementById("title").innerHTML = "Rasilia | Here is Rasilia\'s Personal Website~";
     document.getElementById("Note_and_Show").innerHTML = "Please select the language you wanna use.";
 }
 
 function zh() {
-    document.getElementById("title").innerHTML = "冬风|这里是冬风的个人网站~";
+    document.getElementsByClassName("selected")[0].className = "";
+    document.getElementById("zh").className = "selected";
+    document.getElementById("title").innerHTML = "冬风 | 这里是冬风的个人网站~";
     document.getElementById("Note_and_Show").innerHTML = "选择你要使用的语言";
 }
 
+function mj() {
+    document.getElementsByClassName("selected")[0].className = "";
+    document.getElementById("mj").className = "selected";
+    document.getElementById("title").innerHTML = "Rasilia | ";
+    document.getElementById("Note_and_Show").innerHTML = "";
+}
+
 function jp() {
+    document.getElementsByClassName("selected")[0].className = "";
+    document.getElementById("jp").className = "selected";
     document.getElementById("title").innerHTML = "神木 冬風";
     document.getElementById("Note_and_Show").innerHTML = "";
 }
