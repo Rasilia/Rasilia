b053f94c21f7c503ba39009188bc9ec686899640
Rasilia-冬风 <62973385+Rasilia@users.noreply.github.com>
2022-06-28 04:08:43 +0800

    ~

--- js/lingua.js
+++ js/lingua.js
@@ -1,14 +1,14 @@
 function en() {
     document.getElementById("title").innerHTML = "Rasilia | Here is Rasilia\'s Personal Website~";
-    document.getElementById("header").innerHTML = "Please select the language you wanna use.";
+    document.getElementById("Note_and_Show").innerHTML = "Please select the language you wanna use.";
 }
 
 function zh() {
     document.getElementById("title").innerHTML = "冬风|这里是冬风的个人网站~";
-    document.getElementById("header").innerHTML = "选择你要使用的语言";
+    document.getElementById("Note_and_Show").innerHTML = "选择你要使用的语言";
 }
 
 function jp() {
     document.getElementById("title").innerHTML = "神木 冬風";
-    document.getElementById("header").innerHTML = "";
+    document.getElementById("Note_and_Show").innerHTML = "";
 }
