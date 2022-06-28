4f2981e62a67a4a9049d2268415ca968d5446add
Rasilia-冬风 <62973385+Rasilia@users.noreply.github.com>
2022-06-28 18:52:44 +0800

    ~

--- linguaJS/jp.js
+++ linguaJS/jp.js
@@ -2,7 +2,7 @@ function jp() {
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("jp").className = "selected";
     document.getElementById("title").innerHTML = "神木 冬風";
-    document.getElementById("Note_and_Show").innerHTML = "";
-    document.getElementById("Tip").innerHTML = "";
-    document.getElementById("Confirm").innerHTML = "";
+    document.getElementById("Note_and_Show").innerHTML = "To be translated";
+    document.getElementById("Tip").innerHTML = "To be translated";
+    document.getElementById("Confirm").innerHTML = "To be translated";
 }
--- linguaJS/mj.js
+++ linguaJS/mj.js
@@ -2,7 +2,7 @@ function mj() {
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("mj").className = "selected";
     document.getElementById("title").innerHTML = "Rasilia | ";
-    document.getElementById("Note_and_Show").innerHTML = "";
-    document.getElementById("Tip").innerHTML = "";
-    document.getElementById("Confirm").innerHTML = "";
+    document.getElementById("Note_and_Show").innerHTML = "To be translated";
+    document.getElementById("Tip").innerHTML = "To be translated";
+    document.getElementById("Confirm").innerHTML = "To be translated";
 }
