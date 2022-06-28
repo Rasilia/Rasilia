be9a2af5acd5b26ee1b584c1f15d8d997259a442
Rasilia-冬风 <62973385+Rasilia@users.noreply.github.com>
2022-06-28 16:32:01 +0800

    ~

--- js/goto.js
+++ js/goto.js
@@ -1,3 +1,3 @@
 function GOTO() {
-    $('.selected').document
+    $('.selected').d
 }
--- js/lingua.js
+++ js/lingua.js
@@ -1,14 +1,14 @@
 function en() {
-    document.getElementsByClassName("selected")[0].className = "";
+    document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("en").className = "selected";
-    document.getElementById("title").innerHTML = "Rasilia | Here is Rasilia\'s Personal Website~";
+    document.getElementById("title").innerHTML = "Rasilia | Here Is Rasilia\'s Personal Website~";
     document.getElementById("Note_and_Show").innerHTML = "Please select the language you wanna use.";
     document.getElementById("Tip").innerHTML = "If you have selected, please click the CONFIRM button below.";
     document.getElementById("Confirm").innerHTML = "CONFIRM";
 }
 
 function zh() {
-    document.getElementsByClassName("selected")[0].className = "";
+    document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("zh").className = "selected";
     document.getElementById("title").innerHTML = "冬风 | 这里是冬风的个人网站~";
     document.getElementById("Note_and_Show").innerHTML = "选择你要使用的语言";
@@ -17,7 +17,7 @@ function zh() {
 }
 
 function mj() {
-    document.getElementsByClassName("selected")[0].className = "";
+    document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("mj").className = "selected";
     document.getElementById("title").innerHTML = "Rasilia | ";
     document.getElementById("Note_and_Show").innerHTML = "";
@@ -26,7 +26,7 @@ function mj() {
 }
 
 function jp() {
-    document.getElementsByClassName("selected")[0].className = "";
+    document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("jp").className = "selected";
     document.getElementById("title").innerHTML = "神木 冬風";
     document.getElementById("Note_and_Show").innerHTML = "";
