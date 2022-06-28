c290a15af3e3234cdc13fff48ce17c337d0863eb
Rasilia-冬风 <62973385+Rasilia@users.noreply.github.com>
2022-06-28 10:05:30 +0800

    ~

---
+++ js/goto.js
@@ -0,0 +1,3 @@
+function GOTO() {
+    $('.selected').document
+}
--- js/lingua.js
+++ js/lingua.js
@@ -3,6 +3,8 @@ function en() {
     document.getElementById("en").className = "selected";
     document.getElementById("title").innerHTML = "Rasilia | Here is Rasilia\'s Personal Website~";
     document.getElementById("Note_and_Show").innerHTML = "Please select the language you wanna use.";
+    document.getElementById("Tip").innerHTML = "If you have selected, please click the CONFIRM button below.";
+    document.getElementById("Confirm").innerHTML = "CONFIRM";
 }
 
 function zh() {
@@ -10,6 +12,8 @@ function zh() {
     document.getElementById("zh").className = "selected";
     document.getElementById("title").innerHTML = "冬风 | 这里是冬风的个人网站~";
     document.getElementById("Note_and_Show").innerHTML = "选择你要使用的语言";
+    document.getElementById("Tip").innerHTML = "如果你已选择完毕，请点击下方“确认”按钮";
+    document.getElementById("Confirm").innerHTML = "确认";
 }
 
 function mj() {
@@ -17,6 +21,8 @@ function mj() {
     document.getElementById("mj").className = "selected";
     document.getElementById("title").innerHTML = "Rasilia | ";
     document.getElementById("Note_and_Show").innerHTML = "";
+    document.getElementById("Tip").innerHTML = "";
+    document.getElementById("Confirm").innerHTML = "";
 }
 
 function jp() {
@@ -24,4 +30,6 @@ function jp() {
     document.getElementById("jp").className = "selected";
     document.getElementById("title").innerHTML = "神木 冬風";
     document.getElementById("Note_and_Show").innerHTML = "";
+    document.getElementById("Tip").innerHTML = "";
+    document.getElementById("Confirm").innerHTML = "";
 }
