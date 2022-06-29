e7abcc355ebf9e959c6ec2c009094ef60fe6b9df
Rasilia-冬风 <62973385+Rasilia@users.noreply.github.com>
2022-06-29 18:22:57 +0800

    Update en.js

--- linguaJS/en.js
+++ linguaJS/en.js
@@ -3,6 +3,9 @@ function en() {
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("en").className = "selected";
     document.getElementsByClassName("confirm_1")[0].id = "gotoEN";
+    document.getElementById("gotoEN").onclick = function() {
+        location.href = "lang/en/"
+    }
     //Entry Translation//
     document.getElementById("Title").innerHTML = "Rasilia | Here Is Rasilia\'s Personal Website~";
     document.getElementById("Note_and_Show").innerHTML = "Please select the language you wanna use.<br><p style='font-size: 40%; color: red;'>Tip: If you\'re sure to use English, please switch into other language once and switch back coz I don\'t know how to fix the bug. Sorry!</p>";
