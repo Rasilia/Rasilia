4a84191cbe59a6017428ab375975c54bbf2a67d5
Rasilia-冬风 <62973385+Rasilia@users.noreply.github.com>
2022-06-30 16:46:20 +0800

    ~

--- linguaJS/en.js
+++ linguaJS/en.js
@@ -8,7 +8,7 @@ function en() {
     }
     //Entry Translation//
     document.getElementById("Title").innerHTML = "Rasilia | Here Is Rasilia\'s Personal Website~";
-    document.getElementById("Note_and_Show").innerHTML = "Please select the language you wanna use.<br><p style='font-size: 40%; color: red;'>Tip: If you\'re sure to use English, please switch into other language once and switch back coz I don\'t know how to fix the bug. Sorry!</p>";
+    document.getElementById("Note_and_Show").innerHTML = "Please select the language you wanna use.";
     document.getElementById("Tip").innerHTML = "If you have selected, please click the CONFIRM button below.";
     document.getElementById("gotoEN").innerHTML = "CONFIRM";
 }
