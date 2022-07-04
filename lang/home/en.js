e84e9fbfae0210820ba9ea581de0a2d716426f44
Rasilia-冬风 <62973385+Rasilia@users.noreply.github.com>
2022-07-04 10:32:47 +0800

    ~

--- lang/home/en.js
+++ lang/home/en.js
@@ -1,3 +1,7 @@
 export function lang_home_en() {
     document.getElementById("Title_2").innerHTML = "Rasilia | Here Is Rasilia\'s Personal Website~";
+    document.getElementById("home").innerHTML = "Home";
+    document.getElementById("present_language").innerHTML = "English";
+    document.getElementById("lang").innerHTML = "Language";
+    
 }
--- lang/select/en.js
+++ lang/select/en.js
@@ -1,5 +1,5 @@
 function lang_select_en() {
-    document.getElementById("Title_1").innerHTML = "Rasilia | Select your language";
+    document.getElementById("Title_1").innerHTML = "Rasilia | Select Your Language";
     document.getElementById("Note_and_Show").innerHTML = "Please select the language you wanna use.";
     document.getElementById("Tip").innerHTML = "If you have selected, please click the <i>CONFIRM</i> button below.";
     document.getElementById("Entrance").innerHTML = "CONFIRM";
