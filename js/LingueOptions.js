9f484d38d2fa4ec1f0ed7d4421b949760ee9b14f
Rasilia-冬风 <2634779867@qq.com>
2022-11-10 06:01:35 +0800

    Updates

--- js/LingueOptions.js
+++ js/LingueOptions.js
@@ -4,40 +4,41 @@
 These functions below are used to set language.
 Here is template.
 function LanguageCode() {
-    localStorage.setItem("lang", "LanguageCode")
+    localStorage.setItem("choice", "LanguageCode")
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("LanguageCode").className = "selected";
 }
 */
-localStorage.setItem("lang", "en")
+
+localStorage.setItem("choice", "en")
 
 function en() {
-    localStorage.setItem("lang", "en")
+    localStorage.setItem("choice", "en")
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("en").className = "selected";
 }
 function zh_s() {
-    localStorage.setItem("lang", "zh_s")
+    localStorage.setItem("choice", "zh_s")
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("zh_s").className = "selected";
 }
 function zh_t() {
-    localStorage.setItem("lang", "zh_t")
+    localStorage.setItem("choice", "zh_t")
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("zh_t").className = "selected";
 }
 function it() {
-    localStorage.setItem("lang", "it")
+    localStorage.setItem("choice", "it")
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("it").className = "selected";
 }
 function ja() {
-    localStorage.setItem("lang", "ja")
+    localStorage.setItem("choice", "ja")
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("ja").className = "selected";
 }
 function ml() {
-    localStorage.setItem("lang", "ml")
+    localStorage.setItem("choice", "ml")
     document.getElementsByClassName("selected")[0].className = "unselected";
     document.getElementById("ml").className = "selected";
 }
